908cd010c10732a2bf4a4ff868e7db90e3a63e1b
Ollie Beck <ollieb94@hotmail.co.uk>
2022-09-07 12:34:51 +0100

    Changed array to array of hashes

--- directory.rb
+++ directory.rb
@@ -1,24 +1,30 @@
-#First, we put all student names in an array
-students = [ "Dr. Hannibal Lector", "Darth Vader", "Nurse Ratched", "Michael Corleone",
-    "Alex DeLarge", "The Wicked Witch of the West", "Terminator","Freddy Krueger", 
-    "The Joker", "Joffrey Baratheon", "Norman Bates"]
+#First, we put all student names and in an array of hashes
+student_data = [
+{name: "Dr. Hannibal Lector", cohort: :november}, {name: "Darth Vader", cohort: :november}, 
+{name: "Nurse Ratched", cohort: :november}, {name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november}, {name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november}, {name: "Freddy Krueger", cohort: :november}, 
+{name: "The Joker", cohort: :november}, {name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
+]
 
 #Then, we define methods to make the code easier to read
+  def print_header
   puts "The students of Villain Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]})"
   end
 end
 
-  def print_footer(names)
-puts "Overall, we have #{names.count} great students"
+  def print_footer(students)
+puts "Overall, we have #{students.count} great students"
   end
 
   #Finally, we call the methods
   print_header
-  print(students)
-  print_footer(students)
+  print(student_data)
+  print_footer(student_data)
